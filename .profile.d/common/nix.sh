4d2edfa148c879ecd83235229d8ba77813a75b01
Travis B. Hartwell <nafai@travishartwell.net>
2015-06-10 23:35:45 -0600

    Fix syntax errors

--- .profile.d/common/nix.sh
+++ .profile.d/common/nix.sh
@@ -1,7 +1,7 @@
 debug "Sourcing ~/.profile.d/common/nix.sh"
 
 # If we aren't already on NixOS and nixpkgs is installed
-if [ ! -e /etc/NIXOS] && [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
+if [ ! -e /etc/NIXOS ] && [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
 	. $HOME/.nix-profile/etc/profile.d/nix.sh;
 fi
 
--- .shellrc.d/common/virtualenvwrapper.bash
+++ .shellrc.d/common/virtualenvwrapper.bash
@@ -2,30 +2,31 @@ debug "Sourcing ~/.shellrc.d/common/virtualenvwrapper.bash"
 
 local wrapper_path=$(which virtualenvwrapper.sh)
 
-if [ -z "$(wrapper_path)"]; then
+if [ -z "${wrapper_path}" ]; then
     . "${wrapper_path}"
 
-declare -x virtualenv_prompt
+    declare -x virtualenv_prompt
 
-__virtualenv_prompt_fn()
-{
-    if [ -n "${VIRTUAL_ENV}" ]; then
-        if [ "`basename \"$VIRTUAL_ENV\"`" = "__" ] ; then
-            # special case for Aspen magic directories
-            # see http://www.zetadev.com/software/aspen/
-            virtualenv_prompt="[`basename \`dirname \"$VIRTUAL_ENV\"\``]"
-        else
-            virtualenv_prompt="(`basename \"$VIRTUAL_ENV\"`)"
-        fi
-        echo -ne '\[$(_normal ${yellowf})\]'\
+    __virtualenv_prompt_fn()
+    {
+        if [ -n "${VIRTUAL_ENV}" ]; then
+            if [ "`basename \"$VIRTUAL_ENV\"`" = "__" ] ; then
+                # special case for Aspen magic directories
+                # see http://www.zetadev.com/software/aspen/
+                virtualenv_prompt="[`basename \`dirname \"$VIRTUAL_ENV\"\``]"
+            else
+                virtualenv_prompt="(`basename \"$VIRTUAL_ENV\"`)"
+            fi
+            echo -ne '\[$(_normal ${yellowf})\]'\
 "${virtualenv_prompt}"\
 '\[${_reset}\]'
-    fi
-}
+        fi
+    }
 
-__add_new_prompt_fn __virtualenv_prompt_fn
+    __add_new_prompt_fn __virtualenv_prompt_fn
 
-# Let me handle the prompt
-VIRTUAL_ENV_DISABLE_PROMPT=1
+    # Let me handle the prompt
+    VIRTUAL_ENV_DISABLE_PROMPT=1
 
-export VIRTUAL_ENV_DISABLE_PROMPT virtualenv_prompt
+    export VIRTUAL_ENV_DISABLE_PROMPT virtualenv_prompt
+fi
