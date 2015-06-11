cd43990346ab3b200739d7eb95703df44e6fd860
Travis B. Hartwell <nafai@travishartwell.net>
2015-06-11 17:11:42 -0600

    Add support functions for Nix

--- .profile.d/common/nix.sh
+++ .profile.d/common/nix.sh
@@ -5,4 +5,3 @@ if [ ! -e /etc/NIXOS ] && [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
 	. $HOME/.nix-profile/etc/profile.d/nix.sh;
 fi
 
-# Useful functions and aliases
--- .shellrc.d/common/nix.bash
+++ .shellrc.d/common/nix.bash
@@ -1,6 +1,6 @@
-debug "Sourcing ~/.shellrc.d/common/nix.sh"
+debug "Sourcing ~/.shellrc.d/common/nix.bash"
 
-__nix_prompt_fn()
+function __nix_prompt_fn()
 {
     if [ -n "${IN_NIX_SHELL}" ]; then
         echo -ne '\[$(_normal ${yellowf})\][NIX SHELL]\[${_reset}\]'
@@ -8,3 +8,60 @@ __nix_prompt_fn()
 }
 
 __add_new_prompt_fn __nix_prompt_fn
+
+# Useful functions and aliases
+# Given an executable from $PATH, cd to the package top-level directory
+# in the Nix store
+function cd_nix_package_for_file_dir()
+{
+    if [ "$#" -ne 1 ]; then
+        return $(error 2 "${FUNCNAME} requires a single parameter, \
+a file on the path.")
+    fi
+
+    local f="${1}"
+    local fpath="$(which ${f} 2> /dev/null)"
+
+    if [ -n "${fpath}" ]; then
+        local pkgpath="$(readlink -nf ${fpath} | grep -o '/nix/store/[^/]*')"
+
+        if [ -n "${pkgpath}" ]; then
+            cd "${pkgpath}"
+        else
+            return $(error 2 "${f} not in a package.")
+        fi
+    else
+        return $(error 2 "${f} not found.")
+    fi
+}
+
+# Get the location in the store where .nix files are
+function get_nixpkgs_dir()
+{
+    # Assume the package exists
+    echo $(readlink -nf ${HOME}/.nix-defexpr/channels_root/nixos/nixpkgs)
+}
+
+# cd to the location in the store where .nix files are
+function cd_nixpkgs_dir()
+{
+    local nixpkgs_dir=$(get_nixpkgs_dir)
+
+    cd "${nixpkgs_dir}"
+}
+
+# Find a file in the nixpkgs directory of the store
+function find_file_in_nixpkgs()
+{
+    local nixpkgs_dir=$(get_nixpkgs_dir)
+
+    find "${nixpkgs_dir}" ${*}
+}
+
+# Search, using ag, the files in the nixpkgs directory of the store
+function search_nixpkgs()
+{
+    local nixpkgs_dir=$(get_nixpkgs_dir)
+
+    ag ${*} "${nixpkgs_dir}"
+}
