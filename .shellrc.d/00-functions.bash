7d855de75fd2c2f2a501e70b7536f1d50d1eb188
Travis B. Hartwell <nafai@travishartwell.net>
2015-06-13 02:06:39 -0600

    Add alias for ls

--- .shellrc.d/00-functions.bash
+++ .shellrc.d/00-functions.bash
@@ -1,6 +1,6 @@
-# Useful bash-specific functions needed only for
+# Useful bash-specific functions and aliases needed only for
 # interactive invocations
 
 debug "Sourcing ~/.shellrc.d/00-functions.bash"
 
-# Currently empty, but left as placeholder.
+alias ls='ls --color=auto --group-directories-first'
