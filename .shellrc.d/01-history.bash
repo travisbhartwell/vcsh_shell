c2324a2990eed7b06bef2e3f71948f6ed2c8622f
Travis B. Hartwell <nafai@travishartwell.net>
2015-07-04 22:16:14 -0600

    Change the file that the bash history is saved to.

--- .shellrc.d/01-history.bash
+++ .shellrc.d/01-history.bash
@@ -3,7 +3,9 @@ debug "Sourcing ~/.shellrc.d/01-history.bash"
 HISTCONTROL=ignoreboth
 HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
 HISTIGNORE="&:ls:[bf]g:exit"
-
+# Change history file so it isn't accidentally overwritten in new
+# bash environments
+HISTFILE="${HOME}"/.bash_history_file
 HISTSIZE=10000
 HISTFILESIZE=10000
 
